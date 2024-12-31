a5e02a96b26828775556a1cbe0c4ea2af99b14f9
tinkybeatz <gaedelouis@gmail.com>
2024-12-31 15:29:21 +0100

    help please

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,11 @@
-// import { Component } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
-
-// @Component({
-//   selector: 'app-root',
-//   imports: [RouterOutlet],
-//   templateUrl: './app.component.html',
-// })
-
-// export class AppComponent {
-// }
-
-import { Component, OnInit } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
-import { track } from '@vercel/analytics';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
   imports: [RouterOutlet],
+  templateUrl: './app.component.html',
 })
-export class AppComponent implements OnInit {
-  constructor(private router: Router) {}
 
-  ngOnInit(): void {
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        // Track page view
-        track('pageview', { path: event.urlAfterRedirects });
-      }
-    });
-  }
+export class AppComponent {
 }
