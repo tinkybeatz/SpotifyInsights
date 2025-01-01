2cb00e45f8ae3f20646a26e6a994cc8b4df7b3f0
tinkybeatz <gaedelouis@gmail.com>
2025-01-01 20:20:01 +0100

    gathering url in form (/playlistInsights)

--- src/app/playlist-insights/playlist-insights.component.ts
+++ src/app/playlist-insights/playlist-insights.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   templateUrl: './playlist-insights.component.html',
 })
 export class PlaylistInsightsComponent {
+  constructor() {}
 
+  ngOnInit(): void {}
+
+  submit() {
+    // https://open.spotify.com/playlist/3aI8mappd9DyYvgIkDWy7y?si=c6d807c5eed64387
+    const formInputUrl = (document.getElementById('input') as HTMLInputElement)
+      .value;
+    const playlist_id = formInputUrl.split('playlist/')[1].split('?')[0];
+    console.log(playlist_id);
+  }
 }
+
+// import { Component } from '@angular/core';
+// import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+// @Component({
+//   selector: 'app-playlist-insights',
+//   standalone: true,
+//   templateUrl: './playlist-insights.component.html',
+//   imports: [HttpClientModule], // Import HttpClientModule here
+// })
+// export class PlaylistInsightsComponent {
+//   constructor(private http: HttpClient) {}
+
+//   submit() {
+//     const formInputUrl = (document.getElementById('input') as HTMLInputElement)
+//       .value;
+//     const playlist_id = formInputUrl.split('playlist/')[1].split('?')[0];
+//     console.log(playlist_id);
+//     this.http
+//       .get<any>(`https://api.spotify.com/v1/playlists/${playlist_id}`)
+//       .subscribe((data: any) => {
+//         console.log(data);
+//       });
+//   }
+// }
